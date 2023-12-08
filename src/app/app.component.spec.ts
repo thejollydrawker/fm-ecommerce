38225662f6bc5eb7dc1914c69154f6b9f7566c5f
thejollydrawker <rekaketezeregy@gmail.com>
2023-12-08 15:38:42 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'fm-eccommerce' title`, () => {
+  it(`should have the 'fm-ecommerce' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fm-eccommerce');
+    expect(app.title).toEqual('fm-ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fm-eccommerce');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fm-ecommerce');
   });
 });
