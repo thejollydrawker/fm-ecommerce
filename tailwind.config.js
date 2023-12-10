2f86b707a3969f1206dff11b2b50e749e0591bac
thejollydrawker <rekaketezeregy@gmail.com>
2023-12-10 20:11:13 +0100

    adding styles, responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,27 @@ module.exports = {
     extend: {
       colors: {
         orange: {
-          400: 'hsl(26, 100%, 55%)'
+          400: 'hsl(26, 100%, 55%)',
+          100: 'hsl(25, 100%, 94%)'
+        },
+        black: {
+          900: 'hsl(0, 0%, 0%)'
+        },
+        slate: {
+          200: 'hsl(223, 64%, 98%)'
         }
-    }},
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700
+      },
+      fontSize: {
+        sm: '16px'
+      },
+      fontFamily: {
+        display: ['"Kumbh Sans"', 'sans-serif']
+      }
+    },
     
   },
   plugins: [],
